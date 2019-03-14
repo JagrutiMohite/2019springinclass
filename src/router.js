ed58bbb2ae44f323b99ac8ec828e3752a60fca0e
Jagruti Mohite <mohitejagruti5@gmail.com>
2019-03-14 10:51:51 -0400

    front end

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import MyFriends from './views/MyFriends.vue'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       component: function () { 
         return import(/* webpackChunkName: "about" */ './views/About.vue')
       }
+    },
+    {
+      path: '/myfriends',
+      name: 'myfriends',
+      component: MyFriends
     }
   ]
 })
---
+++ src/views/MyFriends.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+      <h1>My Friends page</h1>
+      <ul>
+          <li>abc</li>
+          <li>def</li>
+          <li>ghi</li>
+          <li>jkl</li>
+      </ul>
+</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
